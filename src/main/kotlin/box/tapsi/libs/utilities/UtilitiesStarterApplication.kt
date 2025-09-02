683bb7a453dc4dbbdc4f923700bb998b72f762a9
mahdibohloul <mahdi.bohloul@tapsi.cab>
2025-09-02 10:17:30 +0330

    Initialize Spring Boot Kotlin project with Gradle configuration, essential dependencies, and project structure

---
+++ src/main/kotlin/box/tapsi/libs/utilities/UtilitiesStarterApplication.kt
@@ -0,0 +1,11 @@
+package box.tapsi.libs.utilities
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UtilitiesStarterApplication
+
+fun main(args: Array<String>) {
+  runApplication<UtilitiesStarterApplication>(*args)
+}
